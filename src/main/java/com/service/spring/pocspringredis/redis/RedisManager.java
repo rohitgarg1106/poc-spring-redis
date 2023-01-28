e52fab47dce0f09adfb1237f1af5fc4d3f564a7e
Rohit Garg <rohit.garg@meesho.com>
2023-01-29 01:49:52 +0530

    STRING-GET,SET

--- src/main/java/com/service/spring/pocspringredis/redis/RedisManager.java
+++ src/main/java/com/service/spring/pocspringredis/redis/RedisManager.java
@@ -4,11 +4,13 @@ import com.service.spring.pocspringredis.common.CommonUtils;
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.RedisURI;
 import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class RedisManager {
 
     private StatefulRedisConnection<String,String> connection;
-
 
     public RedisManager(String redisHost, Integer redisPort, String redisPassword) {
         RedisURI redisURI = RedisURI.builder().withHost(redisHost).withPort(redisPort).build();
@@ -17,6 +19,27 @@ public class RedisManager {
         }
         RedisClient client = RedisClient.create(redisURI);
         this.connection = client.connect();
+    }
+
+    public String getKey(String key){
+        try{
+            RedisCommands<String, String> commands = this.connection.sync();
+            return commands.get(key);
+        }
+        catch (Exception e){
+            log.error("RedisManager::getKey exception {}",e);
+            throw e;
+        }
+    }
+
+    public void setKeyWithExpiry(String key, String value, int expiry){
+        try{
+            RedisCommands<String, String> commands = this.connection.sync();
+            commands.setex(key, expiry, value);
+        }
+        catch (Exception e){
+            log.error("RedisManager::setKeyWithExpiry exception {}",e);
+        }
     }
 
 
